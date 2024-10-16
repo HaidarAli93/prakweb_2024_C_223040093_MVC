154c60ddacf475fc02735f319915bc0896db3241
Aco Koca <haidar.223040093@mail.unpas.ac.id>
2024-10-16 23:48:16 +0700

    Tugas 4: Asset MVC

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-	<h1>About Me</h1>
+<div class="container">
+	<h1 class="mt-4">About Me</h1>
 	<p>Halo, nama saya <?php echo $data['nama']; ?>. Saya adalah seorang <?php echo $data['pekerjaan']; ?>.</p>
+	<img src="<?= BASEURL ?>/img/omori.png" alt="Omori" width="400" class="rounded-circle shadow">
+</div>
