45d1e83cd8d2a268dc27ae4c6449c910fb4b859d
Aco Koca <haidar.223040093@mail.unpas.ac.id>
2024-10-16 16:14:54 +0700

    Tugas 4: Routing MVC

--- app/core/App.php
+++ app/core/App.php
@@ -2,7 +2,17 @@
 
 class App {
 	public function __construct() {
-		echo 'OK!';
+		$url = $this->parseURL();
+		var_dump($url);
+	}
+
+	public function parseURL() {
+		if (isset($_GET['url'])) {
+			$url = ltrim(rtrim($_GET['url'], '/'), '/public/');
+			$url = filter_var($url, FILTER_SANITIZE_URL);
+			$url = explode('/', $url);
+			return $url;
+		}
 	}
 }
 
